ffb8c4336d1a9b32d97648c4e12d9efe749109be
darkcharl <darkcharl@users.noreply.github.com>
2025-05-28 19:36:45 -0400

    fix: uuid script linebreak

--- prep/get_uuids.sh
+++ prep/get_uuids.sh
@@ -2,5 +2,4 @@
 
 sed -En 's/^(new entry "[^"]+"|data "RootTemplate" "[^"]+"|^$)/\1/p' ../PAK/Public/SecretScrolls/Stats/Generated/Data/Object_*.txt | tr -d '"' | awk '{ print $NF }'
 
-#sed -En 's/^(new entry "[^"]+"|data "RootTemplate" "[^"]+"|^$)/\1/p' ../PAK/Public/SecretScrolls/Stats/Generated/Data/Object_*.txt | tr -d '"' | awk '{ print $NF }'
-| egrep '^[a-z0-9]' | sed 's/\(.*\)/"\1",/'
+#sed -En 's/^(new entry "[^"]+"|data "RootTemplate" "[^"]+"|^$)/\1/p' ../PAK/Public/SecretScrolls/Stats/Generated/Data/Object_*.txt | tr -d '"' | awk '{ print $NF }' | egrep '^[a-z0-9]' | sed 's/\(.*\)/"\1",/'
